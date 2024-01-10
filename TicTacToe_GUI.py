02e9a0ae3959e2aa7a7a6959fd0144fe46c43d28
Nasir Nawaz <thebalosh69@gmail.com>
2024-01-10 22:11:18 +0500

    added display

--- TicTacToe_GUI.py
+++ TicTacToe_GUI.py
@@ -1,1 +1,22 @@
-print("This is baloshi69 Tic Tac Toe Game")
+# TicTacToe_GUI.py
+
+"""A tic-tac-toe game built with Python and Tkinter."""
+
+import tkinter as tk
+from tkinter import font
+
+class TicTacToeBoard(tk.Tk):
+    def __init__(self):
+        super.__init__()
+        self.title("Tic-Tac-Toe The game by Baloshi69")
+        self._cells = {}
+
+    def _create_board_display(self):
+        display_frame = tk.Frame(master=self)
+        display_frame.pack(fill=tk.X)
+        self.display = tk.Label(
+            master=display_frame,
+            text="Ready",
+            font=font.Font(size=28, weight="bold"),
+        )
+        self.display.pack()
